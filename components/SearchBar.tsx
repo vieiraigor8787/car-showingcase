d670c6cc8d1fbda162b7813ad993107d22880114
Igor Cerqueira <igor.cerqueira@jussi.com.br>
2023-07-03 11:43:01 -0300

    fix manufacturer, config searchbar, useRouter

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
 import Image from 'next/image'
 
 import SearchManufacturer from './SearchManufacturer'
@@ -19,8 +20,38 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 
 export default function SearchBar() {
   const [manufacturer, setManufacturer] = useState('')
+  const [model, setModel] = useState('')
+  const router = useRouter()
 
-  const handleSearch = () => {}
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    // no data
+    if (manufacturer === '' && model === '') {
+      return alert('Please fill in the search bar')
+    }
+  }
+
+  // having some data
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search)
+
+    if (model) {
+      searchParams.set('model', model)
+    } else {
+      searchParams.delete('model')
+    }
+
+    if (manufacturer) {
+      searchParams.set('manufacturer', manufacturer)
+    } else {
+      searchParams.delete('manufacturer')
+    }
+
+    const newPathName = `${window.location.pathname}?${searchParams.toString()}`
+
+    router.push(newPathName)
+  }
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
@@ -29,9 +60,28 @@ export default function SearchBar() {
           manufacturer={manufacturer}
           setManufacturer={setManufacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
 
-      <SearchButton otherClasses="sm:hidden" />
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="tigyuan"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   )
 }
--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -40,7 +40,7 @@ export default function SearchManufacturer({
           <Combobox.Input
             className="search-manufacturer__input"
             placeholder="Volks"
-            displayValue={(manufacturer: string) => manufacturer}
+            displayValue={(item: string) => item}
             onChange={(e) => setQuery(e.target.value)}
           />
 
@@ -51,7 +51,10 @@ export default function SearchManufacturer({
             leaveTo="opacity-0"
             afterLeave={() => setQuery('')}
           >
-            <Combobox.Options>
+            <Combobox.Options
+              className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+              static
+            >
               {filteredManufacturers.map((item) => (
                 <Combobox.Option
                   key={item}
